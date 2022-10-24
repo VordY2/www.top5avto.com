31097dfc288c6ed187fc59e1ebd4af7750d82715
VordY2 <116597403+VordY2@users.noreply.github.com>
2022-10-24 23:18:55 +0300

    Add files via upload

---
+++ App/scripts/js-script.js
@@ -0,0 +1,31 @@
+var h3Showed = false;
+
+function changeDisplay() {
+	const header3 = document.getElementById("1");
+	header3.style.display=h3Showed ? "block" : "none";
+	h3Showed = !h3Showed;
+}
+var h3Showed2 = false;
+function changeDisplay2() {
+  const header3 = document.getElementById("2");
+  header3.style.display=h3Showed2 ? "block": "none";
+  h3Showed2 = !h3Showed2;
+}
+var h3Showed3 = false;
+function changeDisplay3() {
+  const header3 = document.getElementById("3");
+  header3.style.display=h3Showed3 ? "block": "none";
+  h3Showed3 = !h3Showed3;
+}
+var h3Showed4 = false;
+function changeDisplay4() {
+  const header3 = document.getElementById("4");
+  header3.style.display=h3Showed4 ? "block": "none";
+  h3Showed4 = !h3Showed4;
+}
+var h3Showed5 = false;
+function changeDisplay5() {
+  const header3 = document.getElementById("5");
+  header3.style.display=h3Showed5 ? "block": "none";
+  h3Showed5 = !h3Showed5;
+}
